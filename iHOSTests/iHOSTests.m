145f284c59253380617e21776871da7f7ec9cf61
Chris Martinez <chris.martinez@centricconsulting.com>
2014-04-01 16:59:57 -0400

    Add simple test case and turn on location for testing and debugging

--- iHOSTests/iHOSTests.m
+++ iHOSTests/iHOSTests.m
@@ -26,7 +26,8 @@
 
 - (void)testExample
 {
-    STFail(@"Unit tests are not implemented yet in iHOSTests");
+//    STFail(@"Unit tests are not implemented yet in iHOSTests");
+    STAssertTrue(YES, @"Simple Unit test");
 }
 
 @end
